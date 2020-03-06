296db9b7336f794fbefb2cb0a9c5a3c4885de040
lukieoo <pawkrzysciak@gmail.com>
2020-03-07 00:14:39 +0100

    Add calendar working design 1.5

--- app/src/main/java/com/anioncode/spojrzyj/Classes/BottomSheetDialog.java
+++ app/src/main/java/com/anioncode/spojrzyj/Classes/BottomSheetDialog.java
@@ -2,6 +2,7 @@ package com.anioncode.spojrzyj.Classes;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -17,7 +18,6 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
     public void setupDialog(Dialog dialog, int style) {
         super.setupDialog(dialog, style);
         dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-
         View contentView = View.inflate(getContext(), R.layout.model_bottom_sheet_layout, null);
         dialog.setContentView(contentView);
 
